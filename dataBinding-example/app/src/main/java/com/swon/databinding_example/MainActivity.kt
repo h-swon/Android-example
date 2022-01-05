33539a4ac7bb88a3d4684e41376ab77379174962
h-swon <heosw1107@gmail.com>
2022-01-05 20:40:27 +0900

    DataBinding Basic

--- dataBinding-example/app/src/main/java/com/swon/databinding_example/MainActivity.kt
+++ dataBinding-example/app/src/main/java/com/swon/databinding_example/MainActivity.kt
@@ -2,10 +2,18 @@ package com.swon.databinding_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.swon.databinding_example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.user = User("sungwon", 29)
+        binding.myText = "하이"
     }
 }
---
+++ dataBinding-example/app/src/main/java/com/swon/databinding_example/User.kt
@@ -0,0 +1,6 @@
+package com.swon.databinding_example
+
+data class User(
+    val name: String,
+    val age: Int
+)
